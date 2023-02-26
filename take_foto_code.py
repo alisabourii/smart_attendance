70246199e6e039f717f6134b48c3158bba8a5c2e
THUNDER <72344723+alisabourii@users.noreply.github.com>
2023-02-26 23:02:59 +0300

    Update take_foto_code.py

--- take_foto_code.py
+++ take_foto_code.py
@@ -7,23 +7,24 @@ pygame.camera.init()
   
 # make the list of all available cameras
 camlist = pygame.camera.list_cameras()
-  
-# if camera is detected or not
-for i in range(1,5):
-    if camlist:
-    
-        # initializing the cam variable with default camera
-        cam = pygame.camera.Camera(camlist[1], (640, 480))
-    
-        # opening the camera
-        cam.start()
-        time.sleep(2.0)
-        # capturing the single image
-        image = cam.get_image()
-    
-        # saving the image
-        pygame.image.save(image, "filename{0}.jpg".format(i))
-    
-    # if camera is not detected the moving to else part
-    else:
-        print("No camera on current device")
+
+def take_foto():
+    # if camera is detected or not
+    for i in range(1,3):
+        if camlist:
+        
+            # initializing the cam variable with default camera
+            cam = pygame.camera.Camera(camlist[1], (640, 480))
+        
+            # opening the camera
+            cam.start()
+            time.sleep(2.0)
+            # capturing the single image
+            image = cam.get_image()
+        
+            # saving the image
+            pygame.image.save(image, "filename{0}.jpg".format(i))
+        
+        # if camera is not detected the moving to else part
+        else:
+            print("No camera on current device")
