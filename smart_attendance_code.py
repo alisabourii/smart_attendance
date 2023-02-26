5f2deeef97c83cefd98fc664eac6932171daab6a
THUNDER <72344723+alisabourii@users.noreply.github.com>
2023-02-26 23:06:25 +0300

    Version2

--- smart_attendance_code.py
+++ smart_attendance_code.py
@@ -1,7 +1,15 @@
 import take_foto_code
 import found_faces
 import time
+from datetime import datetime
+
+now = datetime.now()
+clock =now.strftime("%d/%m/%Y %H:%M:%S")
 
 take_foto_code.take_foto()
-time.sleep(1.0)
-found_faces.found_face("filename1")
+time.sleep(0.5)
+
+
+for i in range(1,3):
+    found_faces.found_face("filename{0}".format(i))
+    time.sleep(0.5)
